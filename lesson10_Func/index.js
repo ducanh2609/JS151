fc3f37430768f56ceca734099f81a31b0342e8e0
ducanh2609 <ducanh.nl2@gmail.com>
2024-06-04 22:41:33 +0700

    lesson10

---
+++ lesson10_Func/index.js
@@ -0,0 +1,89 @@
+// const arr = [1, 2, 3]
+// function logArr(arrIn, isTest) {
+//     console.log('arrIn', arrIn)
+//     console.log('isTest', isTest)
+
+//     if (isTest) console.log(isTest)
+//     for (let i = 0; i < arrIn.length; i++) {
+//         console.log(arrIn[i])
+//     }
+//     return 'change'
+// }
+// const testStr = logArr(arr, 'test')
+// console.log(testStr)
+
+// BT:
+// Bài 2: 
+// Viết một hàm nhận vào một mảng số nguyên và một số k, trả về số lớn thứ k trong mảng đó.
+// Điều kiện: k phải <= số lượng phần tử của mảng sau khi loại bỏ phần tử trùng nhau
+// const arr = [2, 5, 2, 7, 9, 10]
+// const k = 3
+// const arr2 = [2, 5, 2, 7, 9, 10, 2, 5, 2, 7, 9, 10]
+
+// function choiceValue(numberArr, key) {
+//     const arrDupli = delDup(numberArr)
+//     arrDupli.sort((a, b) => a - b)
+//     console.log(arrDupli[key - 1]);
+//     return arrDupli[key - 1]
+// }
+
+// function delDup(arrDup) {
+//     for (let i = 0; i < arrDup.length - 1; i++) {
+//         for (let j = i + 1; j < arrDup.length; j++) {
+//             if (arrDup[i] === arrDup[j]) {
+//                 arrDup.splice(j, 1)
+//                 j--
+//             }
+//         }
+//     }
+//     return arrDup
+// }
+
+// choiceValue(arr, k)
+// const arrDel2 = delDup(arr2)
+// console.log(delDup(arr))
+
+// Bài 3: Viết một hàm nhận vào một mảng các số nguyên và trả về số lần xuất hiện nhiều nhất của một số trong mảng đó. Trường hợp có các số có số lần lặp giống nhau thì sẽ in ra số lớn nhất
+const arr = [1, 2, 3, 2, 2, 1, 4, 4, 4, 5, 10, 10, 10]
+
+
+function countMax(arrItem) {
+    const arrCountItem = []
+    let maxObj = {
+        value: arr[0],
+        count: 1,
+    }
+    for (let i = 0; i < arrItem.length - 1; i++) {
+        let count = 1
+        if (!arrCountItem.includes(arrItem[i])) {
+            arrCountItem.push(arrItem[i])
+            for (let j = i + 1; j < arrItem.length; j++) {
+                if (arrItem[i] === arrItem[j]) {
+                    count++
+                }
+            }
+            if (count > maxObj.count) {
+                maxObj = {
+                    value: arrItem[i],
+                    count,
+                }
+            }
+            if (count === maxObj.count) {
+                if (arrItem[i] > maxObj.value) {
+                    maxObj = {
+                        value: arrItem[i],
+                        count,
+                    }
+                }
+            }
+        }
+    }
+    return maxObj
+}
+
+function checkMaxObj() {
+
+}
+console.log(countMax(arr))
+const arr2 = [1, 2, 3, 2, 2, 1, 4, 4, 4, 5, 10, 10, 10]
+countMax(arr2)
--- lesson9_Arr-Obj/index.js
+++ lesson9_Arr-Obj/index.js
@@ -1,19 +1,19 @@
-const arr1 = [2, 3, 4, 5]
-const arr2 = ['Quân', 'Huy', 'Phương', 'Trung']
-// const arr3 = [[], []]
-// const arr4 = [{}, {}]
+// const arr1 = [2, 3, 4, 5]
+// const arr2 = ['Quân', 'Huy', 'Phương', 'Trung']
+// // const arr3 = [[], []]
+// // const arr4 = [{}, {}]
 
 
-const QuangInfo = {
-    Name: 'Quang',
-    Age: 20,
-    City: 'Hà Nội',
-    Love: ['Yến', 'Linh'],
-    Car: {
-        Name: 'Mescerdes',
-        Level: 'S',
-    }
-}
+// const QuangInfo = {
+//     Name: 'Quang',
+//     Age: 20,
+//     City: 'Hà Nội',
+//     Love: ['Yến', 'Linh'],
+//     Car: {
+//         Name: 'Mescerdes',
+//         Level: 'S',
+//     }
+// }
 
 //1. Các method sử dụng với array
 // + Lấy độ dài: <arr>.length
@@ -63,3 +63,124 @@ const QuangInfo = {
 // obj['<key>']
 // const key = ['Name', 'Age']
 // console.log(QuangInfo[key[1]])
+
+
+// BT:
+// Bài 1: Đếm Số Lần Xuất Hiện trong Mảng:
+// const numbers = [1, 2, 3, 4, 2, 1, 3, 2, 4, 2];
+// const targetCounting = 2;
+// let count = 0
+// for (let i = 0; i < numbers.length; i++) {
+//     if (targetCounting === numbers[i]) {
+//         count++
+//     }
+// }
+// console.log(count)
+// console.log(numbers.includes(6)) // kiểm tra xem 1 gtri có là phần tử của 1 mảng hay k
+// console.log(numbers.indexOf(6)) // tìm index của 1 phần tử trong mảng, nếu k tìm thấy trả về -1
+
+// Bài 2: Tìm Số Lớn Nhất Trong Mảng:
+// const numbers = [5, 2, 9, 3, 7, 11, 8];
+// let max = 0
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] > max) {
+//         max = numbers[i]
+//     }
+// }
+
+// console.log(max)
+// Bài 3: Đảo Ngược Mảng:
+// const array = [1, 2, 3, 4, 5];
+// const newArr = []
+// for (let i = array.length - 1; i >= 0; i--) {
+//     newArr.push(array[i])
+// }
+// console.log(newArr)
+// const array2 = [1, 2, 3, 10, 8, 4, 5];
+// const arrSort = array2.sort((a, b) => b - a)
+// console.log(arrSort)
+
+
+// Bài 4: Xóa Phần Tử Trùng Nhau Khỏi Mảng:
+// const duplicatesArray = [1, 2, 3, 2, 4, 4, 5];
+// C1:
+// const newArr = []
+// for (let i = 0; i < duplicatesArray.length; i++) {
+//     if (!newArr.includes(duplicatesArray[i])) {
+//         newArr.push(duplicatesArray[i])
+//     }
+// }
+// console.log(newArr)
+// C2:
+// for (let i = 0; i < duplicatesArray.length - 1; i++) {
+//     for (let j = i + 1; j < duplicatesArray.length; j++) {
+//         if (duplicatesArray[i] === duplicatesArray[j]) {
+//             duplicatesArray.splice(j, 1)
+//             j--
+//         }
+//     }
+// }
+
+// console.log(duplicatesArray);
+
+// Bài 5: Tính Điểm Trung Bình từ Mảng Đối Tượng:
+// const students = [
+//     { name: "Alice", age: 20, grades: [80, 85, 90] },
+//     { name: "Bob", age: 21, grades: [75, 88, 95] }
+// ];
+
+// const avgArr = []
+// for (let i = 0; i < students.length; i++) {
+//     const grades = students[i].grades
+//     let sum = 0
+//     for (let j = 0; j < grades.length; j++) {
+//         sum += grades[j]
+//     }
+//     const avg = sum / grades.length
+//     avgArr.push({
+//         name: students[i].name,
+//         avg,
+//     })
+// }
+
+// console.log(avgArr)
+
+// Bài 6: Tìm Sản Phẩm Rẻ Nhất Trong Mảng Đối Tượng:
+// const products = [
+//     { name: "Áo", price: 20 },
+//     { name: "Quần", price: 30 },
+//     { name: "Giày", price: 10 }
+// ];
+
+// let minPrice = products[0].price
+// let minProduct = products[0]
+// for (let i = 1; i < products.length; i++) {
+//     if (products[i].price < minPrice) {
+//         minPrice = products[i].price
+//         minProduct = products[i]
+//     }
+// }
+
+// console.log(minProduct);
+
+
+// Bài 7: Thực hiện nhóm các điểm số của các học sinh lại thành đối tượng: tên môn học, danh sách điểm
+// const students = [
+//     { name: "Alice", age: 20, grades: { math: 90, english: 85, science: 88 } },
+//     { name: "Bob", age: 21, grades: { math: 80, english: 75, science: 78 } }
+// ]
+
+// const pointTable = {
+//     math: [],
+//     english: [],
+//     science: []
+// }
+// for (let i = 0; i < students.length; i++) {
+//     const grades = students[i].grades
+//     pointTable.math.push(grades.math)
+//     pointTable.english.push(grades.english)
+//     pointTable.science.push(grades.science)
+// }
+
+
+// console.log(pointTable)
