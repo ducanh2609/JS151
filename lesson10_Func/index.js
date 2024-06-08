cb1eb0426b8591382b281c7413d67be94944b58f
ducanh2609 <ducanh.nl2@gmail.com>
2024-06-08 19:44:38 +0700

    lesson10 _update

--- lesson10_Func/index.js
+++ lesson10_Func/index.js
@@ -13,7 +13,7 @@
 // console.log(testStr)
 
 // BT:
-// Bài 2: 
+// Bài 2:
 // Viết một hàm nhận vào một mảng số nguyên và một số k, trả về số lớn thứ k trong mảng đó.
 // Điều kiện: k phải <= số lượng phần tử của mảng sau khi loại bỏ phần tử trùng nhau
 // const arr = [2, 5, 2, 7, 9, 10]
@@ -44,46 +44,65 @@
 // console.log(delDup(arr))
 
 // Bài 3: Viết một hàm nhận vào một mảng các số nguyên và trả về số lần xuất hiện nhiều nhất của một số trong mảng đó. Trường hợp có các số có số lần lặp giống nhau thì sẽ in ra số lớn nhất
-const arr = [1, 2, 3, 2, 2, 1, 4, 4, 4, 5, 10, 10, 10]
+// const arr = [1, 2, 3, 2, 2, 1, 4, 4, 4, 5, 10, 10, 10]
 
 
-function countMax(arrItem) {
-    const arrCountItem = []
-    let maxObj = {
-        value: arr[0],
-        count: 1,
-    }
-    for (let i = 0; i < arrItem.length - 1; i++) {
-        let count = 1
-        if (!arrCountItem.includes(arrItem[i])) {
-            arrCountItem.push(arrItem[i])
-            for (let j = i + 1; j < arrItem.length; j++) {
-                if (arrItem[i] === arrItem[j]) {
-                    count++
-                }
-            }
-            if (count > maxObj.count) {
-                maxObj = {
-                    value: arrItem[i],
-                    count,
-                }
-            }
-            if (count === maxObj.count) {
-                if (arrItem[i] > maxObj.value) {
-                    maxObj = {
-                        value: arrItem[i],
-                        count,
-                    }
-                }
-            }
-        }
-    }
-    return maxObj
-}
+// function countMax(arrItem) {
+//     const arrCountItem = []
+//     let maxObj = {
+//         value: arr[0],
+//         count: 1,
+//     }
+//     for (let i = 0; i < arrItem.length - 1; i++) {
+//         let count = 1
+//         if (!arrCountItem.includes(arrItem[i])) {
+//             arrCountItem.push(arrItem[i])
+//             for (let j = i + 1; j < arrItem.length; j++) {
+//                 if (arrItem[i] === arrItem[j]) {
+//                     count++
+//                 }
+//             }
+//             if (count > maxObj.count) {
+//                 maxObj = {
+//                     value: arrItem[i],
+//                     count,
+//                 }
+//             }
+//             if (count === maxObj.count) {
+//                 if (arrItem[i] > maxObj.value) {
+//                     maxObj = {
+//                         value: arrItem[i],
+//                         count,
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     return maxObj
+// }
 
-function checkMaxObj() {
+// console.log(countMax(arr))
+// const arr2 = [1, 2, 3, 2, 2, 1, 4, 4, 4, 5, 10, 10, 10]
+// countMax(arr2)
+// Bài 4: Tách các từ trong một chuỗi thành một mảng các chuỗi.
+// function splitWords(str) {
+//     return str.split(' ')
+// }
+// console.log(splitWords("This is a test"));
 
-}
-console.log(countMax(arr))
-const arr2 = [1, 2, 3, 2, 2, 1, 4, 4, 4, 5, 10, 10, 10]
-countMax(arr2)
+// Bài 5: Tính tổng của tất cả các giá trị là số trong đối tượng
+// const data = { a: 1, b: 'hello', c: 3, d: 'world' }
+
+// function sumNumber(obj) {
+//     const valueList = Object.values(obj)
+//     let sum = 0
+//     for (let i = 0; i < valueList.length; i++) {
+//         if (typeof valueList[i] === 'number') {
+//             sum += valueList[i]
+//         }
+//     }
+//     return sum
+// }
+
+// const sum = sumNumber(data)
+// console.log(sum)
