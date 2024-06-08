cabc4689bd29960726f2c4073abb649ce6cbae40
ducanh2609 <ducanh.nl2@gmail.com>
2024-06-08 22:29:56 +0700

    lesson11

--- lesson10_Func/index.js
+++ lesson10_Func/index.js
@@ -85,6 +85,12 @@
 // const arr2 = [1, 2, 3, 2, 2, 1, 4, 4, 4, 5, 10, 10, 10]
 // countMax(arr2)
 // Bài 4: Tách các từ trong một chuỗi thành một mảng các chuỗi.
+// + Tách string thành 1 mảng các kí tự: split() - tạo 1 mảng có 1 phần tử là string đó
+// - split('') : trả ra 1 mảng có các phần tử là các kí tự của string đó
+// - split(' '): trả ra 1 mảng có các phần tử là chuỗi được cắt ra theo chuỗi cắt truyền vào
+// const str = 'This is test'
+// const arr = str.split('test')
+// console.log(arr)
 // function splitWords(str) {
 //     return str.split(' ')
 // }
@@ -92,6 +98,10 @@
 
 // Bài 5: Tính tổng của tất cả các giá trị là số trong đối tượng
 // const data = { a: 1, b: 'hello', c: 3, d: 'world' }
+// const arrKeys = Object.keys(data)
+// const arrValues = Object.values(data)
+// console.log('arrKeys', arrKeys)
+// console.log('arrValues', arrValues)
 
 // function sumNumber(obj) {
 //     const valueList = Object.values(obj)
@@ -106,3 +116,19 @@
 
 // const sum = sumNumber(data)
 // console.log(sum)
+
+// * Sort:
+// const arr = [3, 1, 4, 2] // sau lần 1 của j: [1,3,4,2] =>  sau lần 2 của i  [1,2,4,3] =>  [1,2,3,4]
+// C1:
+// for (let i = 0; i < arr.length - 1; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//         if (arr[i] < arr[j]) {
+//             let temp = arr[i]
+//             arr[i] = arr[j]
+//             arr[j] = temp
+//         }
+//     }
+
+// }
+// arr.sort((a, b) => b - a)
+// console.log(arr)
---
+++ lesson11_DOM/index.js
@@ -0,0 +1,79 @@
+// Bài 1: Thay đổi nội dung thẻ qua DOM
+// const title = document.getElementsByClassName('title')
+// Khi gọi theo class thì nó luôn có dạng giống với 1 mảng
+// title[0].innerHTML = 'Đây là nội dung đã được thay đổi'
+// const title = document.getElementById('title')
+// console.log('title', title)
+
+// title.innerHTML = 'Đây là nội dung đã được thay đổi'
+// Khi gọi theo id thì nó gọi chính xác 1 thẻ
+// title.innerHTML = 'Đây là nội dung'
+
+// Bài 2: Thay đổi css 1 thẻ qua DOM
+// title.style.fontSize = '40px'
+// title.style.color = 'red'
+// title.style.backgroundColor = 'blue'
+
+// Bài 3: Click button chuyển đổi 2 trạng thái
+// const title = document.getElementById('title')
+
+// const originStyle = {
+//     color: title.style.color,
+//     backgroundColor: title.style.backgroundColor
+// }
+// const changeColor = {
+//     color: 'red',
+//     backgroundColor: 'blue',
+// }
+// let isNewStyle = false
+// function changeStyle(element) {
+//     console.log('isNewStyle', isNewStyle)
+//     if (!isNewStyle) {
+//         element.style.color = changeColor.color
+//         element.style.backgroundColor = changeColor.backgroundColor
+//     } else {
+//         element.style.color = originStyle.color
+//         element.style.backgroundColor = originStyle.backgroundColor
+//     }
+
+//     isNewStyle = !isNewStyle
+// }
+// const btnClick = document.getElementById('btnClick')
+// btnClick.addEventListener('click', () => {
+//     changeStyle(title)
+// })
+
+// Bài 4 - 5: 
+const input = document.getElementById('input')
+const button = document.getElementById('button')
+const todoList = document.getElementById('todo-list')
+
+let todos = localStorage.getItem('todos') // get data trên local storage theo key
+console.log(todos)
+if (todos) {
+    todos = JSON.parse(todos)
+} else {
+    todos = []
+}
+for (let i = 0; i < todos.length; i++) {
+    todoList.innerHTML += `
+                <div>
+                    <input type="checkbox">
+                    <span>${todos[i]}</span>
+                </div>
+        `
+}
+
+button.addEventListener('click', () => {
+    todos.push(input.value)
+    localStorage.setItem('todos', JSON.stringify(todos))
+    if (input.value?.trim()) {
+        todoList.innerHTML += `
+                <div>
+                    <input type="checkbox">
+                    <span>${input.value}</span>
+                </div>
+        `
+        input.value = ''
+    }
+})
